abf7230a3f7a24fcac1f3fd86c4e9fb46c12e571
Varun Sharma <varun.sharma@progress.com>
2021-08-12 17:27:03 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# haproxy_lb
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: haproxy_lb
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
